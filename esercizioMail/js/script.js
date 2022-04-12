624ea35d7b03ac59382743956ce5b050b8766b97
Cix8 <francesco.natal_fn@libero.it>
2022-04-12 13:46:58 +0200

    'I see you writing numbers!!!'

--- esercizioMail/js/script.js
+++ esercizioMail/js/script.js
@@ -1,11 +1,16 @@
 const emailList = ['franco@mymail.com','ciccio@email.it','piero@libero.it','gino@gmail.com','anonimo@nomail.com'];
 const userEmail = prompt('Inserisci la tua email');
 
-let result = 'Spiacente, non sei in lista...';
+let result = 'I dati inseriti non sono validi';
 
-for (let i = 0; i < emailList.length; i++) {
-    if (userEmail === emailList[i]) {
-        result = `La tua email ${userEmail} è presente nella mia lista, puoi accedere!!!`;
+let dataValidityCheck = parseInt(userEmail);
+
+if (isNaN(dataValidityCheck)) {
+    result = 'Spiacente, non sei in lista...';
+    for (let i = 0; i < emailList.length; i++) {
+        if (userEmail === emailList[i]) {
+            result = `La tua email ${userEmail} è presente nella mia lista, puoi accedere!!!`;
+        }
     }
 }
 
