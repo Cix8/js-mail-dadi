f5563c79d78f5f9a20b949dfcf6d42b2c08119a1
Cix8 <francesco.natal_fn@libero.it>
2022-04-12 13:28:34 +0200

    add email program

--- esercizioMail/js/script.js
+++ esercizioMail/js/script.js
@@ -1,1 +1,12 @@
-alert('ciao');
+const emailList = ['franco@mymail.com','ciccio@email.it','piero@libero.it','gino@gmail.com','anonimo@nomail.com'];
+const userEmail = prompt('Inserisci la tua email');
+
+let result = 'Spiacente, non sei in lista...';
+
+for (let i = 0; i < emailList.length; i++) {
+    if (userEmail === emailList[i]) {
+        result = `La tua email ${userEmail} è presente nella mia lista, puoi accedere!!!`;
+    }
+}
+
+console.log(result);
